710ca53a978bb2269084cc20ebe756542b46dd27
Jatan-Mathasoliya <jatan.codinggita@gmail.com>
2025-01-22 22:14:04 +0530

    Controllers folder contains all the logic of Authentication and Profile setup.

--- Blink-Chat Server/Controllers/AuthController.js
+++ Blink-Chat Server/Controllers/AuthController.js
@@ -12,37 +12,3 @@ const createToken = (email, userId) => {
 };
 
 
-// This is code of Signup Logic that handles user's Signup
-export const signup = async (request, response) => {
-    try {
-      const { email, password } = request.body;
-      if (!email || !password) {
-        return response.status(400).json({ message: "Email and password are required" });
-      }
-  
-      console.log("Original Password:", password);
-  
-      // Save the user directly without manually hashing the password
-      const user = await User.create({ email, password });
-      console.log("User Saved:", user);
-  
-      // Set the JWT token as a cookie
-      response.cookie('jwt', createToken(email, user.id), {
-        maxAge,
-        secure: true,
-        sameSite: "None",
-      });
-  
-      response.status(201).json({
-        user: {
-          id: user.id,
-          email: user.email,
-          profileSetup: user.profileSetup,
-        },
-      });
-    } catch (error) {
-      console.error(error);
-      return response.status(500).send("Internal server error");
-    }
-  };
-  
