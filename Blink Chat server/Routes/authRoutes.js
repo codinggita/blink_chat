db490017fcaec3d4a72faf21b6732e51182c8db1
Jatan-Mathasoliya <jatan.codinggita@gmail.com>
2025-01-23 11:42:51 +0530

    Adding a Login route in AuthRoutes.js File with login Function call.

--- Blink Chat server/Routes/authRoutes.js
+++ Blink Chat server/Routes/authRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
 
-import {signup}  from "../controllers/AuthController.js"; // Import the signup controller function from the controllers Folder.
+import {signup, login}  from "../controllers/AuthController.js"; // Import the signup controller function from the controllers Folder.
 
 const authRoutes = Router(); // Initialize a new router instance
 
-authRoutes.post("/signup", signup); // Define a POST route for "/signup" that calls the signup controller
+authRoutes.post("/signup", signup); // Define a POST route for "/signup" that calls the signup Function.
+authRoutes.post("/login", login); // Define a POST route for "/Login" that calls the login Function.
 
 
 export default authRoutes; // // Export the router to use it in other parts of the application
