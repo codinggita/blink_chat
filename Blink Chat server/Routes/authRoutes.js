8dee96e9b40f409f62a3f46d3d773decc8eaf347
Jatan-Mathasoliya <jatan.codinggita@gmail.com>
2025-02-02 11:24:24 +0530

    Adding a Logout function that used to logout from chat page to Login/Signup page.

--- Blink Chat server/Routes/authRoutes.js
+++ Blink Chat server/Routes/authRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 
-import {signup, login, getUserInfo, updateProfile, addProfileImage, removeProfileImage}  from "../controllers/AuthController.js"; // Import all the functions from the AuthController.js file (For authentication and profile setup.)
+import {signup, login, getUserInfo, updateProfile, addProfileImage, removeProfileImage, logOut}  from "../controllers/AuthController.js"; // Import all the functions from the AuthController.js file (For authentication and profile setup.)
 
 import { verifyToken } from "../middleware/AuthMiddleware.js"; // import the verifyToken function from middleware (That also shown in this commit.)
 
@@ -13,6 +13,7 @@ authRoutes.get('/user-info', verifyToken, getUserInfo) // Define a GET route for
 authRoutes.post('/update-profile', verifyToken, updateProfile) // Define a POST route for "/update-profile" that calls the updateProfile Function and taake all details from user using POST request.
 authRoutes.post('/add-profile-image', verifyToken, upload.single('profile-image') , addProfileImage) // Define a POST route for upload a progile image in frontend and also store image address in database.
 authRoutes.delete('/remove-profile-image', verifyToken, removeProfileImage) // Define a DELETE route for "/remove-profile-image" that calls the removeProfileImage Function.
+authRoutes.post('/logout', logOut) // Define s post route for logout function that used to logout user from the application.
 
 
 export default authRoutes; // // Export the router to use it in other parts of the application
